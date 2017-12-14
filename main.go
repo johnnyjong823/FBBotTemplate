7ca5fa4c39d3cbdb2a11417dcbd4b9637e64fe9c
johnnyjong823 <johnnyjong823@gmail.com>
2017-12-14 15:33:59 +0800

    Update main.go

--- main.go
+++ main.go
@@ -28,13 +28,6 @@ func main() {
 	mess.AccessToken = os.Getenv("TOKEN")
 	log.Println("Bot start in token:", mess.VerifyToken)
 	mess.MessageReceived = MessageReceived
-
-	err := mess.SetWelcomeMessage(&SendMessage{
-		Text: "hello!",
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
 	
 	http.HandleFunc("/webhook", mess.Handler)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
@@ -48,10 +41,21 @@ func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
 		fmt.Println(err)
 		return
 	}
-	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("Hello   , %s %s, %s", profile.FirstName, profile.LastName, msg.Text))
-	if err != nil {
-		fmt.Println(err)
+	answers := []string{"洞么參在此誰敢放肆","就跟你說要有外野了",
+						    "洞么參剛到步校","打手槍次數是零",
+						    "欸幹嘛不要這樣啊","都欺負我","欸我不是二分隊的啊",
+						    "我沒有要簽啊","小心我把你們埋進土裡",
+						    "013 洞么參 1800我告訴我家狗我想尿尿它要帶我去，預計1830回家",
+						    "冒牌洞么參呢？","我一身哈味","小姐 要不要援交?","看甚麼看 在看把你埋進土裡",
+						    "你千萬要相信2000字的心得","洞洞么你的頭怎麼那麼大？"}
+	if strings.Contains(msg.Text, "哈哈哈"){
+		var txt =answers[rand.Intn(len(answers))] 
+		resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("%s", txt))
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
+	
 	
 	//resp, err := mess.SetWelcomeMessage(SendMessage{Text: msg.Text})
 	
